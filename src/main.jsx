97cdba695664a13b93579b369c05ec59e7d97130
Ankit paul <ankit@LAPTOP-29SAFFIC.(none)>
2024-11-30 17:24:14 +0530

    started adding basic redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import './index.css';
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+
+import { Toaster } from 'react-hot-toast';
+import {Provider} from 'react-redux';
+
+import App from './App.jsx';
+import store from './store.js';
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
+    <Toaster></Toaster>
+    </BrowserRouter>
+    </Provider>
 );
+
 
 
